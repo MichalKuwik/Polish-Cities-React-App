be061f0149f88f53de60596cc39d3cafdf97b52f
Michał Kuwik <Michal7074@poczta.fm>
2019-08-18 12:24:31 +0200

    add icons to scores of items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import styles from './Item.module.scss';
 import PropTypes from 'prop-types';
+import surfaceImg from '../../assets/img/soil.png';
+import populImg from '../../assets/img/family.png';
+import voivodeshipImg from '../../assets/img/map.png';
 
 function Item({city}){
   return(
     <div className={styles.item}>
       <p className={styles.header}>{city.name}</p>
-      <p className={styles.info}>Wojewódźtwo: {city.voivodeship}</p>
-      <p className={styles.info}>Populacja: {city.population} mieszkańców</p>
-      <p className={styles.info}>Obszar: {city.surface} km²</p>
+      <p className={styles.info}><img 
+          className={styles.imgIcon} 
+          src={voivodeshipImg} 
+          alt=''
+          />Wojewódźtwo: {city.voivodeship}</p>
+      <p className={styles.info}>
+        <img 
+          className={styles.imgIcon} 
+          src={populImg} 
+          alt=''
+          />Populacja: {city.population} mieszkańców
+      </p>
+      <p className={styles.info}>
+        <img 
+          className={styles.imgIcon} 
+          src={surfaceImg} 
+          alt=''
+          />Obszar: {city.surface} km²
+      </p>
       <img 
         src={city.img} 
         alt="img"
