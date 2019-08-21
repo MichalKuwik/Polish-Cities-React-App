bbfa3ff79fbc71ee00bf4dae08a1642b5bcb8081
Michał Kuwik <Michal7074@poczta.fm>
2019-08-21 23:55:38 +0200

    add content and styled in about component

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './About.module.scss';
 
 const About = () => (
-  <h2>This is about page</h2>
+  <section className={styles.section}>
+    <h2 className={styles.header}>Słowem wstępu...</h2>
+    <p className={styles.descr}>
+      Witam w aplikacji 'Największe Polskie Miasta'
+      Stworzyłem API które zawiera 160 miast.
+      Jeżeli chcesz proszę ściągnij plik i swobodnie możesz korzystać w swoich projektach.
+      Mam nadzieję ,że dzieki temu pojektowi choć trochę odświeżysz wiedzę odnośnie Naszego kraju.
+      Jeżeli niema w API miasta w którym mieszkasz, lub jest Ci bliskie , nie gniewaj się ;)
+      Miłej zabawy ;)
+    </p>
+  </section>
+  
 )
 
 export default About;
