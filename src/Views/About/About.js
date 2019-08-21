d2e3f4bd7d500adf603c40a0fc74aabb7f233863
Michał Kuwik <Michal7074@poczta.fm>
2019-08-22 00:39:53 +0200

    add animation in components: about,search, edit styles of button,display info-warning

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -5,12 +5,14 @@ const About = () => (
   <section className={styles.section}>
     <h2 className={styles.header}>Słowem wstępu...</h2>
     <p className={styles.descr}>
-      Witam w aplikacji 'Największe Polskie Miasta'
+      Witaj w aplikacji<br /> <span className={styles.span}>
+        'Polskie Miasta'</span>
+        <br/>
       Stworzyłem API które zawiera 160 miast.
-      Jeżeli chcesz proszę ściągnij plik i swobodnie możesz korzystać w swoich projektach.
+      <br />Jeżeli chcesz proszę ściągnij plik i swobodnie możesz korzystać w swoich projektach.<br/>
       Mam nadzieję ,że dzieki temu pojektowi choć trochę odświeżysz wiedzę odnośnie Naszego kraju.
-      Jeżeli niema w API miasta w którym mieszkasz, lub jest Ci bliskie , nie gniewaj się ;)
-      Miłej zabawy ;)
+      <br/>Jeżeli niema w API miasta w którym mieszkasz, lub jest Ci bliskie , nie gniewaj się ;)
+      <br/>Pamiętaj to tylko zabawa ;)
     </p>
   </section>
   
